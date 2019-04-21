c6046040145087111e1d4f0a7ba8077d9b5dcf59
Shannon Archer <shannon.archer0@gmail.com>
2019-04-21 00:19:32 -0500

    return count and individual level data

--- server/api/levels.js
+++ server/api/levels.js
@@ -4,12 +4,28 @@ const Level = require('../models/level');
 
 const router = express.Router();
 
-/* GET home page. */
+/* GET levels. */
 router.get('/', async (req, res) => {
   db.connect();
   const levels = await Level.find({}, 'id primaryWord order');
   db.close();
   res.json(levels);
+});
+
+/* GET level count. */
+router.get('/count', async (req, res) => {
+  db.connect();
+  const levelCount = await Level.countDocuments();
+  db.close();
+  res.json(levelCount);
+});
+
+/* GET level count. */
+router.get('/:id', async (req, res) => {
+  db.connect();
+  const levelCount = await Level.findOne({ id: req.params.id });
+  db.close();
+  res.json(levelCount);
 });
 
 module.exports = router;

56570bfe2a0a68e85a71b2b58a206d4b402770cb
Shannon Archer <shannon.archer0@gmail.com>
2019-04-26 22:51:44 -0500

    mongoose model validation

--- server/models/level.js
+++ server/models/level.js
@@ -1,10 +1,28 @@
 const mongoose = require('mongoose');
 
 module.exports = mongoose.model('levels', {
-  id: String,
-  primaryWord: String,
-  targetWords: Array,
-  relatedWords: Array,
-  seed: Number,
-  order: Number,
+  id: {
+    type: String,
+    required: true,
+  },
+  primaryWord: {
+    type: String,
+    required: true,
+  },
+  targetWords: {
+    type: Array,
+    required: true,
+  },
+  relatedWords: {
+    type: Array,
+    required: true,
+  },
+  seed: {
+    type: Number,
+    required: true,
+  },
+  order: {
+    type: Number,
+    required: true,
+  },
 });
--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 
 module.exports = mongoose.model('users', {
-  id: String,
-  name: String,
+  id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 4,
+    maxlength: 16,
+  },
 });
